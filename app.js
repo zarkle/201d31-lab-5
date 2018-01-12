0c89424faadaf93d20405fd93f80edd779567464
Beverly Pham <beverly.pham@gmail.com>
2018-01-12 14:20:10 -0800

    problem 3 added

--- app.js
+++ app.js
@@ -52,11 +52,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumOfFirstNumbers = sum(a,b)[0];
+  var sumOfThreeNumbers = sum(sumOfFirstNumbers,c)[0];
+  var productOfFirstNumbers = multiply(a,b)[0];
+  var productOfThreeNumbers = multiply(productOfFirstNumbers,c)[0];
+  var messageSum = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThreeNumbers + '.';
+  var messageProduct = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOfThreeNumbers + '.';
+  return [sumOfThreeNumbers, productOfThreeNumbers, messageSum, messageProduct];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
